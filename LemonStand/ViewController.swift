8ffbdfb30077cb1e8c2d27b233ea12b5819e71d9
Eliot Arntz <arntzel@gmail.com>
2014-07-23 17:53:05 -0400

    Initial Commit

---
+++ LemonStand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LemonStand
+//
+//  Created by Eliot Arntz on 7/23/14.
+//  Copyright (c) 2014 Bitfountain.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
